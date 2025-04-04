cef1a5d14558e28968542df91987006a74ccf7fd
Nhlanhla-N <siyabongangubeni8@gmail.com>
2025-04-04 23:52:59 +0200

    Add Bows' public key and update code.c

--- tutorial1/app/code.c
+++ tutorial1/app/code.c
@@ -1,9 +1,10 @@
 // INCLUDES
 #include <stdio.h>
 #include "code.h"
+#include <stdlib.h>
 
 // GLOBALS
-
+#define MAX_LENGTH 256
 
 //FUNCTIONS
 // welcome message
@@ -15,17 +16,28 @@ void welcome_message()
 void get_name(char* name)
 {
     printf("Enter group name : ");
-    gets(name);
+   fgets(name, MAX_LENGTH, stdin);
 }
 
 // 1. Function to create a folder using the name variable as its name
 void create_folder(char* name)
 {
-
+	char command[256];
+	snprintf(command, sizeof(command), "mkdir -p %s", name);
+	system(command);
 }
 
 // 2. Function to create a file in the folder <folder_name> called group.txt where you insert the <group_name> as text
 void create_file(char* folder_name, char* group_name)
 {
-
+    char filepath[512];
+    snprintf(filepath, sizeof(filepath), "%s/group.txt", folder_name);
+    
+    FILE *file = fopen(filepath, "w");
+    if (file != NULL) {
+        fprintf(file, "%s\n", group_name);
+        fclose(file);
+    } else {
+        perror("Error creating file");
+    }
 }
